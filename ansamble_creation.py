c4e432bc66318dbb497a4e1f61d630e30a6e0a24
Edi Muskardin <edi.muskardin@silicon-austria.com>
2022-11-14 11:27:27 +0100

    create new branch

---
+++ ansamble_creation.py
@@ -0,0 +1,5 @@
+
+
+# returns a dictionary where key is cluster label and value is corresponding mdp
+def compute_ansamble_mdp(alergia_traces,optimize_for='accuracy',  skip_sequential_clusers=False,):
+    pass
