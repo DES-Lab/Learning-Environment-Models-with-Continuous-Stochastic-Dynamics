fe2d82b67b52c301d8bc7cb7e114288af9a3c3f5
Edi Muskardin <edi.muskardin@silicon-austria.com>
2023-04-28 08:32:39 +0200

    push final cartpole results (5 iterations)

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if os.name == 'nt':
 else:
     aalpy.paths.path_to_prism = "~/Programs/prism-4.7-linux64/bin/prism"
 
-env_name = "Acrobot-v1"
+env_name = "CartPole-v1"
 
 agents = None
 agent_names = None
@@ -40,53 +40,54 @@ else:
 num_clusters_per_env = {'Acrobot-v1': 128, 'LunarLander-v2': 512,
                         'MountainCar-v0': 128, 'CartPole-v1': 128}
 
-num_traces = 1000
-num_clusters = num_clusters_per_env[env_name]
-include_randomness_in_sampling = True
-load_all = False
+for i in range(10):
+    num_traces = 2500
+    num_clusters = num_clusters_per_env[env_name]
+    include_randomness_in_sampling = True
+    load_all = False
 
-env = gym.make(env_name, )
-traces_file_name = f'{env_name}_{num_traces}_traces'
+    env = gym.make(env_name, )
+    traces_file_name = f'{env_name}_{num_traces}_traces'
 
-randomness = (0, 0.05, 0.1, 0.15, 0.2) if include_randomness_in_sampling else (0,)
+    randomness = (0, 0.05, 0.1, 0.15, 0.2) if include_randomness_in_sampling else (0,)
 
-traces = get_traces_from_policy(agent, env, num_episodes=num_traces, agent_name='DQN',
-                                randomness_probabilities=(0, 0.05, 0.1, 0.15, 0.2), load_traces=load_all)
+    traces = get_traces_from_policy(agent, env, num_episodes=num_traces, agent_name='DQN',
+                                    randomness_probabilities=(0, 0.05, 0.1, 0.15, 0.2), load_traces=load_all)
 
-obs, actions = get_observations_and_actions(traces)
+    obs, actions = get_observations_and_actions(traces)
 
-dim_red_pipeline = None
-if env_name == 'MountainCar-v0':
-    dim_red_pipeline = PipelineWrapper(env_name, num_traces,
-                                       [('powerTransformer', PowerTransformer()), ], load_pipeline=load_all)
-if env_name == 'LunarLander-v2':
-    dim_red_pipeline = PipelineWrapper(env_name, num_traces, [
-        ('manualMapper', LunarLanderManualDimReduction()),
-        ('powerTransformer', PowerTransformer()), ], load_pipeline=load_all)
-if env_name == 'CartPole-v1':
-    dim_red_pipeline = PipelineWrapper(env_name, num_traces,
-                                       [('powerTransformer', PowerTransformer()), ], load_pipeline=load_all)
-if env_name == 'Acrobot-v1':
-    dim_red_pipeline = PipelineWrapper(env_name, num_traces, [('powerTransformer', PowerTransformer()),
-                                                              ('lda_2', LinearDiscriminantAnalysis(n_components=2))],
-                                       load_pipeline=load_all)
-    # dim_red_pipeline = PipelineWrapper(env_name, num_traces,
-    #                                    [('powerTransformer', PowerTransformer()), ], load_pipeline=load_all)
+    dim_red_pipeline = None
+    if env_name == 'MountainCar-v0':
+        dim_red_pipeline = PipelineWrapper(env_name, num_traces,
+                                           [('powerTransformer', PowerTransformer()), ], load_pipeline=load_all)
+    if env_name == 'LunarLander-v2':
+        dim_red_pipeline = PipelineWrapper(env_name, num_traces, [
+            ('manualMapper', LunarLanderManualDimReduction()),
+            ('powerTransformer', PowerTransformer()), ], load_pipeline=load_all)
+    if env_name == 'CartPole-v1':
+        dim_red_pipeline = PipelineWrapper(env_name, num_traces,
+                                           [('powerTransformer', PowerTransformer()), ], load_pipeline=load_all)
+    if env_name == 'Acrobot-v1':
+        dim_red_pipeline = PipelineWrapper(env_name, num_traces, [('powerTransformer', PowerTransformer()),
+                                                                  ('lda_2', LinearDiscriminantAnalysis(n_components=2))],
+                                           load_pipeline=load_all)
+        # dim_red_pipeline = PipelineWrapper(env_name, num_traces,
+        #                                    [('powerTransformer', PowerTransformer()), ], load_pipeline=load_all)
 
-# fit and transform concrete traces
-dim_red_pipeline.fit(obs, actions)
-transformed = dim_red_pipeline.transform(obs)
-# get clustering function
-k_means_clustering, cluster_labels = get_k_means_clustering(transformed, num_clusters, dim_red_pipeline.pipeline_name,
-                                                            load_fun=load_all)
-# create abstract traces
-abstract_traces = create_abstract_traces(env_name, traces, cluster_labels)
-# get initial model
-model = run_JAlergia(abstract_traces, automaton_type='mdp', path_to_jAlergia_jar='alergia.jar', heap_memory='-Xmx12G',
-                     optimize_for='accuracy')
+    # fit and transform concrete traces
+    dim_red_pipeline.fit(obs, actions)
+    transformed = dim_red_pipeline.transform(obs)
+    # get clustering function
+    k_means_clustering, cluster_labels = get_k_means_clustering(transformed, num_clusters, dim_red_pipeline.pipeline_name,
+                                                                load_fun=load_all)
+    # create abstract traces
+    abstract_traces = create_abstract_traces(env_name, traces, cluster_labels)
+    # get initial model
+    model = run_JAlergia(abstract_traces, automaton_type='mdp', path_to_jAlergia_jar='alergia.jar', heap_memory='-Xmx12G',
+                         optimize_for='accuracy')
 
-ir = IterativeRefinement(env, env_name, model, abstract_traces, dim_red_pipeline, k_means_clustering,
-                         scheduler_type='probabilistic', experiment_name_prefix='exp2')
+    ir = IterativeRefinement(env, env_name, model, abstract_traces, dim_red_pipeline, k_means_clustering,
+                             scheduler_type='probabilistic', experiment_name_prefix=f'A_exp{i}')
 
-# run iterative refinement
-results = ir.iteratively_refine_model(12, 50)
+    # run iterative refinement
+    results = ir.iteratively_refine_model(15, 50)
