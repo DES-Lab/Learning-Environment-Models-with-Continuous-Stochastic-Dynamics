97e5018c6976311540e405b6b97e52cff36cc089
Edi Muskardin <edi.muskardin@silicon-austria.com>
2023-05-08 09:20:23 +0200

    update visualization_util.py

--- visualization_util.py
+++ visualization_util.py
@@ -14,6 +14,23 @@ def tikzplotlib_fix_ncols(obj):
         tikzplotlib_fix_ncols(child)
 
 
+#     iteration_round_data = defaultdict(list)
+#     goal_reached_per = defaultdict(list)
+#     tot_sum = []
+#     for e in experiments:
+#         ts = 0
+#         for iter_round, data in e.items():
+#             iteration_round_data[iter_round].extend(data['all_rewards'])
+#             gp = sum([1 if i >= 195 else 0 for i in data['all_rewards']])
+#             ts += sum(data['all_rewards'])
+#             goal_reached_per[iter_round].append(gp)
+#         tot_sum.append(ts)
+#
+#     print(tot_sum)
+#     print(mean(tot_sum), stdev(tot_sum))
+#
+#     exit()
+
 def get_iteration_averages(experiments, method):
     assert method in {'mean_stddev', 'median_quantiles'}
     if method == 'median_quantiles':
@@ -52,7 +69,7 @@ def visualize_experiment_runs(experiments, env_name, method, baseline_val=None):
     plot_value_1 = np.array(list(plot_value_1.values()))
     plot_value_2 = np.array(list(plot_value_2.values()))
 
-    refinement_rounds = list(range(1, refinement_rounds + 2))
+    refinement_rounds = list(range(1, len(experiments[0].keys()) + 1))
 
     fig = plt.figure()
 
@@ -70,6 +87,9 @@ def visualize_experiment_runs(experiments, env_name, method, baseline_val=None):
     plt.title(f'{env_name}: {len(refinement_rounds)} Iterations of {episodes_per_iter} Episodes')
 
     plt.show()
+    # import tikzplotlib
+    # tikzplotlib_fix_ncols(fig)
+    # tikzplotlib.save("figures/tmp.tex")
 
 
 def visualize_multiple_experiments(experiment_1, experiment_2, env_name, method, baseline_val=None):
@@ -95,11 +115,13 @@ def visualize_multiple_experiments(experiment_1, experiment_2, env_name, method,
     plt.fill_between(refinement_rounds, exp_2_plot_val_1 - exp_2_plot_val_2, exp_2_plot_val_1 + exp_2_plot_val_2,
                      color='b', alpha=0.2)
 
-    if baseline_val is not None:
+    if env_name == 'Lunar Lander':
         # plt.plot(refinement_rounds, [baseline_val] * len(refinement_rounds), 'g-', label='RL baseline')
         plt.plot(refinement_rounds, [136] * len(refinement_rounds), 'gd', label='sb3-dqn')
         plt.plot(refinement_rounds, [181] * len(refinement_rounds), 'gx', label='sb3-a2c')
         plt.plot(refinement_rounds, [223] * len(refinement_rounds), 'g*', label='sb3-ppo')
+    else:
+        plt.plot(refinement_rounds, [baseline_val] * len(refinement_rounds), 'g-', label='RL baseline')
 
     plt.xlabel('Refinement Round')
     plt.ylabel('Reward')
@@ -113,6 +135,41 @@ def visualize_multiple_experiments(experiment_1, experiment_2, env_name, method,
     # import tikzplotlib
     # tikzplotlib_fix_ncols(fig)
     # tikzplotlib.save("figures/lunar_lander_comparison_mean.tex")
+
+
+def visualize_multiple_experiments2(experiments, env_name, method, baseline_val=None):
+    refinement_rounds = list(range(1, len(experiments[0][1][0].keys()) + 1))
+
+    fig = plt.figure()
+
+    for exp_name, data in experiments:
+        exp_1_plot_val_1, exp_1_plot_val_2 = get_iteration_averages(data, method)
+        exp_1_plot_val_1 = np.array(list(exp_1_plot_val_1.values()))
+        exp_1_plot_val_2 = np.array(list(exp_1_plot_val_2.values()))
+
+        plt.plot(refinement_rounds, exp_1_plot_val_1, label=f'Mean Reward: {exp_name}')
+        plt.fill_between(refinement_rounds, exp_1_plot_val_1 - exp_1_plot_val_2, exp_1_plot_val_1 + exp_1_plot_val_2
+                         , alpha=0.2)
+
+    if env_name == 'Lunar Lander':
+        plt.plot(refinement_rounds, [136] * len(refinement_rounds), 'gd', label='sb3-dqn')
+        plt.plot(refinement_rounds, [181] * len(refinement_rounds), 'gx', label='sb3-a2c')
+        plt.plot(refinement_rounds, [223] * len(refinement_rounds), 'g*', label='sb3-ppo')
+    else:
+        plt.plot(refinement_rounds, [baseline_val] * len(refinement_rounds), 'g', label='Baseline Val')
+
+    plt.xlabel('Refinement Round')
+    plt.ylabel('Reward')
+    plt.legend()
+
+    episodes_per_iter = experiments[0][1][0][0]["episodes_per_iteration"]
+    plt.title(f'{env_name}: {len(refinement_rounds)} Iterations of {episodes_per_iter} Episodes')
+
+    plt.show()
+
+    # import tikzplotlib
+    # tikzplotlib_fix_ncols(fig)
+    # tikzplotlib.save("figures/cartpole_multiple_comparison_mean.tex")
 
 
 def visualize_goal_and_crash(data, env_name):
@@ -138,12 +195,30 @@ def load_all(files):
 
 
 if __name__ == '__main__':
-    cartpole_files = [
+    cartpole_128_clusters = [
         f'pickles/results/final_exp{i}_CartPole-v1_num_traces_2500_powerTransformer_n_clusters_128_ri_15_ep_50.pk'
         for i in range(5)]
-    mountain_car_files = [
+    cartpole_64_clusters = [
+        f'pickles/results/cp_64_{i}_CartPole-v1_num_traces_2500_powerTransformer_n_clusters_64_ri_15_ep_50.pk'
+        for i in range(5)]
+    cartpole_32_clusters = [
+        f'pickles/results/cp_test_{i}_CartPole-v1_num_traces_2500_powerTransformer_n_clusters_32_ri_15_ep_50.pk'
+        for i in range(6)]
+
+    mountain_car_256 = [
         f'pickles/results/A_exp{i}_MountainCar-v0_num_traces_2500_powerTransformer_n_clusters_256_ri_25_ep_50.pk'
         for i in [0, 1, 2, 3, 6]]
+    mountain_car_128 = [
+        f'pickles/results/mc_128_exp_{i}_MountainCar-v0_num_traces_2500_powerTransformer_n_clusters_128_ri_25_ep_50.pk'
+        for i in range(5)
+    ]
+    mountain_car_64 = [
+        f'pickles/results/mc_64_exp_{i}_MountainCar-v0_num_traces_2500_powerTransformer_n_clusters_64_ri_25_ep_50.pk'
+        for i in range(5)]
+    mountain_car_96 = [
+        f'pickles/results/mc_96_exp_{i}_MountainCar-v0_num_traces_2500_powerTransformer_n_clusters_96_ri_25_ep_50.pk'
+        for i in range(5)]
+
     acrobot_file_lda = [
         f'pickles/results/A_exp{i}_Acrobot-v1_num_traces_2500_powerTransformer_lda_2_n_clusters_256_ri_25_ep_50.pk'
         for i in range(10)]
@@ -157,25 +232,33 @@ if __name__ == '__main__':
         f'pickles/results/mexp{i + 1}_LunarLander-v2_num_traces_2500_manualMapper_powerTransformer_n_clusters_1024_ri_25_ep_50.pk'
         for i in range(5)]
 
-    baseline_values = {'MountainCar': -105, 'Acrobot': - 100, 'Cartpole': 200, 'LunarLander': 250}
+    baseline_values = {'MountainCar': -130, 'Acrobot': - 100, 'Cartpole': 200, 'LunarLander': 250}
 
-    experiment = 'LunarLander'
+    experiment = 'MountainCar'
     avg_method = 'mean_stddev'  # 'median_quantiles'
 
-    all_experiments = [load(l) for l in acrobot_file_manual]
+    # all_experiments = [load(l) for l in mountain_car_128]
+    #
+    # visualize_experiment_runs(all_experiments, 'Exp name', avg_method, )
+    # # get_max_rew_and_std(all_experiments)
+    #
+    # visualize_multiple_experiments2([('CP 128', load_all(cartpole_128_clusters)), ('64', load_all(cartpole_64_clusters)), ('CP 32',load_all(cartpole_32_clusters))], 'Cartpole', avg_method, 200)
+    # exit()
 
     if experiment == 'LunarLander':
         visualize_multiple_experiments(('Manual Mapper', load_all(lunar_lander_manual)),
                                        ('LDA', load_all(lunar_lander_lda)),
-                                       env_name='LunarLander', method=avg_method,
+                                       env_name='Lunar Lander', method=avg_method,
                                        baseline_val=baseline_values[experiment])
     if experiment == 'Acrobot':
         visualize_multiple_experiments(('Manual Mapper', load_all(acrobot_file_manual)),
                                        ('LDA', load_all(acrobot_file_lda)),
                                        env_name='Acrobot', method=avg_method, baseline_val=baseline_values[experiment])
     if experiment == 'MountainCar':
-        visualize_experiment_runs(load_all(mountain_car_files), experiment, avg_method,
-                                  baseline_val=baseline_values[experiment])
+        visualize_multiple_experiments2([('k=256', load_all(mountain_car_256)),
+                                         ('k=128', load_all(mountain_car_128)),
+                                         ('k=64',load_all(mountain_car_64))], 'Mountain Car', avg_method, -130)
     if experiment == 'Cartpole':
-        visualize_experiment_runs(load_all(cartpole_files), experiment, avg_method,
-                                  baseline_val=baseline_values[experiment])
+        visualize_multiple_experiments2([('k=128', load_all(cartpole_128_clusters)),
+                                         ('k=64', load_all(cartpole_64_clusters)),
+                                         ('k=32',load_all(cartpole_32_clusters))], 'Cartpole', avg_method, 200)
